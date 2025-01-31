59055115cf67eff7602e46988e2fe302c9ee6d8b
wiam <wiamlembardi233@gmail.com>
2025-01-31 16:00:04 +0100

    code html et css

---
+++ add.js
@@ -0,0 +1,20 @@
+let Title =document.getElementById('Title');
+let Author=document.getElementById('Author');
+let Year=document.getElementById('Year');
+let Type=document.getElementById('Type');
+let URL=document.getElementById('URL');
+let submit=document.getElementById('s')
+
+
+console.log(Title,Author,Year,Type,URL)
+
+// create product
+let 
+// save localstorage
+// clear input
+// read
+// count 
+// delete
+//update
+// search
+// clean data
